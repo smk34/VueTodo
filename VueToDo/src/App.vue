3921fd6c21e15b4dadd88e3ab44fab9808e0a029
Soumik Sarkar <20475126+smk34@users.noreply.github.com>
2020-12-12 02:30:14 +0530

    Add files via upload

---
+++ VueToDo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>LCO</h1>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+export default {
+  name: "App",
+  components: {
+    TodoList
+  }
+};
+</script>
+
+
+<style >
+body {
+  background-color: rgb(66, 66, 66);
+  color: aliceblue;
+}
+</style>
